69b0d60a29a6c1f198dc7633616681283357e509
Guilherme W. Domeneghini <163318408+guilhermedomeneghini@users.noreply.github.com>
2025-08-12 13:43:50 -0300

    init commit

---
+++ Anotacoes.md
@@ -0,0 +1,2 @@
+## Iniciar projeto em react: 
+`npx create-react-app react_calc_imc --template typescript`
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import styles from './App.module.css'
+import poweredImage from './assets/powered.png'
+import { calculateImc, Level, levels } from './helpers/imc';
+import { GridItem } from './components/GridItem/GridItem';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+const App = () => {
+  const [heightField, setHeightField] = useState<number>(0);
+  const [weightField, setWeightField] = useState<number>(0);
+  const [toShow, setToShow] = useState<Level | null> (null);
+
+  const handleCalculateButton = ()=>{
+    if(heightField && weightField){
+      setToShow(calculateImc(heightField, weightField));
+    } else {
+      alert("Digite todos os campos.")
+    }
+  }
+
+   return(
+    <div className={styles.main}>
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={poweredImage} alt="" width={"150px"}/>
+        </div>
       </header>
+
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+          <h1>Calcule o seu IMC</h1>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aliquam tempora dolor tenetur quibusdam autem facilis quo, beatae necessitatibus at, quaerat rem doloribus molestias. Necessitatibus possimus doloribus dolore exercitationem nihil hic.</p>
+          
+          <input
+            type='number'
+            placeholder='Digite a sua altura. Ex: 1.5 (em metros)'
+            value={heightField > 0 ? heightField : ''}
+            onChange={e => setHeightField(parseFloat(e.target.value))}
+          />
+
+          <input
+            type='number'
+            placeholder='Digite o seu peso. Ex: 75.3 (em kg)'
+            value={weightField > 0 ? weightField: ''}
+            onChange={e => setWeightField(parseFloat(e.target.value))}
+          />
+
+          <button onClick={handleCalculateButton}>Calcular</button>
+
+
+        </div>
+        <div className={styles.rightSide}>
+          {!toShow &&
+            <div className={styles.grid}>
+              {levels.map((item,key)=>(
+                <GridItem key={key} item={item}/>
+              ))}
+            </div>
+          }
+
+          {toShow &&
+            <div className={styles.rightBig}>
+              <div className={styles.rightArrow}></div>
+              <GridItem item={toShow}/>
+            </div>
+          }
+        </div>
+      </div>
     </div>
-  );
+   );
 }
 
 export default App;
